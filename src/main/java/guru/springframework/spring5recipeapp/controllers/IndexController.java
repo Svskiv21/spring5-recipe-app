9e422227e9f92cdf67f377a4c28fcb438287ca95
Svskiv21 <nataliakrukar@gmail.com>
2021-12-12 18:21:40 +0100

    added Category class, Difficulty enum and repositories

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
-        System.out.println("Some message to say... 1234");
         return "index";
     }
 }
